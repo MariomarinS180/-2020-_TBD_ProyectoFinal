92b5f9f6f760a908ec476ba1d06f193f544144c1
Mario Marin Ramirez <mariomarin502t@gmail.com>
2020-11-19 22:53:42 -0600

    Se agregó el código correspondiente para realizar la conexion a la BD de postegreSQL

--- ProyectoFinal_ABCC/src/proyectofinal_abcc/ProyectoFinal_ABCC.java
+++ ProyectoFinal_ABCC/src/proyectofinal_abcc/ProyectoFinal_ABCC.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectofinal_abcc;
 
-/**
- *
- * @author marin
- */
+import java.sql.*;
 public class ProyectoFinal_ABCC {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/BD_Biblioteca"; 
+        String usuario = "mmarin"; 
+        String contrasenia = "03082000s"; 
+        
+        try {
+            Class.forName("org.postgresql.Driver"); 
+            Connection conexion = DriverManager.getConnection(url, usuario, contrasenia); 
+            java.sql.Statement st = conexion.createStatement(); 
+            String sql 
+                    = "SELECT id_registrador, nombre, apellido_paterno, apellido_materno FROM registrador"; 
+            ResultSet rs  = st.executeQuery(sql); 
+            while(rs.next()){
+                String id = rs.getString("id_registrador"); 
+                String nombre = rs.getString("nombre"); 
+                String primerap = rs.getString("apellido_paterno"); 
+                String segundoap = rs.getString("apellido_materno"); 
+                 System.out.println("ID: " + id + " <<Nombre: " + nombre + " PrimerAP: " + primerap + " SegundoAP: "+ segundoap ); 
+            }
+            rs.close();
+            st.close();
+            conexion.close();
+        } catch (Exception e) {
+            System.out.println("ERROR DE CONEXION " + e.getMessage());
+        }
+        
+    }
+ 
 }
